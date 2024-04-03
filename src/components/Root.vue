66b5c62264c7688d5da884d985291d7e0eaf245c
Jacob Mills <jrmills@hotmail.co.uk>
2024-04-03 17:12:31 +0100

    Replaced template vue project

---
+++ src/components/Root.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="root">
+    </div>
+</template>
+
+<style scoped>
+.root {
+    background-color: #202020;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+</style>
